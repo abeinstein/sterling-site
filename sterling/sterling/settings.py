e812f097808b4a55a97c2c78fe78322c356045cb
Andrew <abeinstein@uchicago.edu>
2013-09-11 17:45:07 -0500

    Added south databae adapter

--- sterling/sterling/settings.py
+++ sterling/sterling/settings.py
@@ -178,6 +178,7 @@ DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO', 'https')
 
+SOUTH_DATABASE_ADAPTERS = {'default':'south.db.postgresql_psycopg2'}
 
 
 
