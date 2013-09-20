69080c9137c31bffc4e8086ae552a1662856c706
Andrew <abeinstein@uchicago.edu>
2013-09-19 20:31:50 -0700

    Modified settings to fix db on heroku

--- sterling/sterling/settings.py
+++ sterling/sterling/settings.py
@@ -175,14 +175,14 @@ LOGIN_REDIRECT_URL = '/apps/'
 
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
 
-if not DEBUG:
-    import dj_database_url
+# if not DEBUG:
+import dj_database_url
 
-    DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
-    SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO', 'https')
 
-    SOUTH_DATABASE_ADAPTERS = {'default':'south.db.postgresql_psycopg2'}
+SOUTH_DATABASE_ADAPTERS = {'default':'south.db.postgresql_psycopg2'}
 
 
 
