709400e93367cb4b73baf9dbe9984ca247db3705
mlevy <mlevy@uchicago.edu>
2013-09-23 17:16:46 -0500

    Modified user ids in facebook messaging

--- sterling/fbNodes/facebookMessages.py
+++ sterling/fbNodes/facebookMessages.py
@@ -12,7 +12,8 @@ class SendMsgsBot(sleekxmpp.ClientXMPP):
 	def start(self, event):
 		self.send_presence()
 		self.get_roster()
-		for recipient in self.recipients:
+		for recipient_id in self.recipients:
+			recipient = '-' + str(recipient_id) + '@chat.facebook.com'
 			self.send_message(mto=recipient, mbody=self.message, mtype='chat')
 		
 		self.disconnect(wait=True)
@@ -20,9 +21,12 @@ class SendMsgsBot(sleekxmpp.ClientXMPP):
 
 
 def send_facebook_messages(inviter_id, invited_list, message, o_auth_token):
-	xmpp = SendMsgsBot(inviter_id, invited_list, message)
+	inviter = str(inviter_id) + '@chat.facebook.com'
+	xmpp = SendMsgsBot(inviter, invited_list, message)
 	xmpp.credentials['api_key'] = '422143484562527'
 	xmpp.credentials['access_token'] = o_auth_token
 		
 	if xmpp.connect(server):
 		xmpp.process(block=True)
+	else:
+		print "Error"
