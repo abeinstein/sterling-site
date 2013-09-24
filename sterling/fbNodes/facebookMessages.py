e38ad686d37f6f5dbbf0c27489f0c3f4304fa280
mlevy <mlevy@uchicago.edu>
2013-09-23 19:27:21 -0500

    evaluated a string

--- sterling/fbNodes/facebookMessages.py
+++ sterling/fbNodes/facebookMessages.py
@@ -1,5 +1,6 @@
 import sleekxmpp
 import logging
+import ast
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -21,12 +22,13 @@ class SendMsgsBot(sleekxmpp.ClientXMPP):
 			recipient = '-' + str(recipient_id) + '@chat.facebook.com'
 			self.send_message(mto=recipient, mbody=self.message, mtype='chat')
 		
-		self.disconnect()
+		self.disconnect(wait=True)
 	
 
 
 def send_facebook_messages(inviter_id, invited_list, message, o_auth_token):
 	inviter = str(inviter_id) + '@chat.facebook.com'
+	invited_list = eval(invited_list)
 	xmpp = SendMsgsBot(inviter, invited_list, message)
 	xmpp.credentials['api_key'] = '422143484562527'
 	xmpp.credentials['access_token'] = o_auth_token
