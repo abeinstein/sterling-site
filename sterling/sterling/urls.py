67cab6848410cd218d5402031596f5a34f454e4b
Andrew <abeinstein@uchicago.edu>
2013-09-25 20:48:51 -0500

    Added app create form and admin

--- sterling/sterling/urls.py
+++ sterling/sterling/urls.py
@@ -1,24 +1,25 @@
 from django.contrib.auth.views import login, logout
 from django.conf.urls import patterns, include, url
 from splash.views import SplashFormView
-from apps.views import AppListView, SterlingRegistrationView
+from apps.views import AppListView, AppCreateView, SterlingRegistrationView
 
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
+# TODO: Separate urls into different apps
 urlpatterns = patterns('',
     # Examples:
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', SplashFormView.as_view(), name="index"),
     url(r'^login/$', login, {'template_name': 'login.html'}, name="login"),
     url(r'^logout/$', logout, {'template_name': 'logout.html'}, name="logout"),
     url(r'^apps/$', AppListView.as_view(), name="dashboard"),
+    url(r'^apps/create/$', AppCreateView.as_view(), name="create_app"),
     url(r'^accounts/register/', SterlingRegistrationView.as_view(), name="registration_register"),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 
