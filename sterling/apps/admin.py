a4fc56618d561a27e27101a1c4474a8f4684c326
mlevy <mlevy@uchicago.edu>
2013-10-02 17:37:11 -0500

    Updated admin file to reflect model changes

--- sterling/apps/admin.py
+++ sterling/apps/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import MobileApp, Membership
+from .models import MobileApp, DevMembership
 
 admin.site.register(MobileApp)
-admin.site.register(Membership)
+admin.site.register(DevMembership)
