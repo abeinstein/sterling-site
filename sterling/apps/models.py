64113737b0ff3994840fc1b0435d2fe6f20c8554
Andrew <abeinstein@uchicago.edu>
2013-10-02 19:13:41 -0500

    Added stats, fixed some models stuff

--- sterling/apps/models.py
+++ sterling/apps/models.py
@@ -4,7 +4,7 @@ import datetime
 from django.conf import settings
 from django.db import models
 
-# from fbNodes.models import InvitationNode, FbNode
+from suggestions.models import AppUser, AppUserMembership, Suggestion
 
 class MobileApp(models.Model):
     ''' Represents an app that is signed up for the platform '''
@@ -17,34 +17,37 @@ class MobileApp(models.Model):
     def __unicode__(self):
         return self.name
 
+    # def algorithms_used(self):
+    #     return Algorithm.objects.filter()
+
     def num_users(self, datetime=datetime.datetime.now()):
         ''' Returns number of users signed up for the app
         Optional datetime parameter that gets number of users before that datetime.
         '''
-        return FbNode.objects.filter(apps=self, created__lt=datetime).count()
+        return AppUserMembership.objects.filter(mobile_app=self, created__lt=datetime).count()
 
+    # TODO: Investigate these queries to see if they're fast (they're probably not)
+    def num_suggested(self, datetime=datetime.datetime.now()):
+        ''' Returns number of invitations that were clicked, with optional datetime parameter '''
+        return Suggestion.objects.filter(suggestion_list__app_user_membership__mobile_app=self,
+                                        created__lt=datetime).count()
 
     def num_invited(self, datetime=datetime.datetime.now()):
         ''' Returns number of people invited to the app
         Optional datetime parameter that gets number of users invited before that datetime. 
         '''
-        return InvitationNode.objects.filter(app=self, 
-                                            created__lt=datetime,
-                                            ).count()
-    def num_invitations_clicked(self, datetime=datetime.datetime.now()):
-        ''' Returns number of invitations that were clicked, with optional datetime parameter '''
-        return InvitationNode.objects.filter(app=self, 
-                                            link_clicked=True,
-                                            created__lt=datetime).count()
+        return Suggestion.objects.filter(suggestion_list__app_user_membership__mobile_app=self,
+                                         invited=True,
+                                         created__lt=datetime).count()
+
+
 
     def num_invitations_joined(self, datetime=datetime.datetime.now()):
         ''' Returns number of invitations that resulted in app signups '''
-        return InvitationNode.objects.filter(app=self,
-                                            joined=True,
+        return Suggestion.objects.filter(suggestion_list__app_user_membership__mobile_app=self,
+                                            accepted=True,
                                             created__lt=datetime).count()
 
-    # def average_number_of_invites_per_user(self, datetime=datetime.datetime.now()):
-    #     '''  '''
 
 
 class DevMembership(models.Model):
