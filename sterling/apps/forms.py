4ebebf4b3e95bac59171583fa3d634ad5a538ceb
mlevy <mlevy@uchicago.edu>
2013-11-10 10:32:17 -0600

    More updates on da settings page

--- sterling/apps/forms.py
+++ sterling/apps/forms.py
@@ -2,11 +2,23 @@ from django.forms import ModelForm, Select
 
 from apps.models import AppSettings
 
-class AppSettingsForm(ModelForm):
+CITIES = (  ("NOT SET", "N/A"),
+            ("ATLANTA", "ATL"),
+            ("BOSTON", "BOS"),
+            ("CHICAGO", "CHI"),
+            ("HOUSTON", "HOU"),
+            ("LOS ANGELES", "LA"),
+            ("NEW YORK", "NY"),
+            ("SAN FRANCISCO", "SF"),
+            ("SEATTLE", "SEA"))
 
+
+
+class AppSettingsForm(ModelForm):
     class Meta:
         model = AppSettings
-        fields = ['likes_sports', 'likes_books', 'likes_music', 'likes_restaurants', 'likes_games', 'political_bias']
+        fields = ['likes_sports', 'likes_books', 'likes_music', 'likes_restaurants', 'likes_games', 'political_bias', 'city', 'social_circle']
         widgets = {
-            'political_bias': Select(choices=AppSettings.POLITICAL_CHOICES)
+            'political_bias': Select(choices=AppSettings.POLITICAL_CHOICES),
+            'city': Select(choices=CITIES),
         }
