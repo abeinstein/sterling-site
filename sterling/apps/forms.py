bc419f1e773ad869e98f463b6bac1c47ea5fed56
mlevy <mlevy@uchicago.edu>
2013-11-10 10:12:28 -0600

    Updated settings view

--- sterling/apps/forms.py
+++ sterling/apps/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Select
 
 from apps.models import AppSettings
 
@@ -6,4 +6,7 @@ class AppSettingsForm(ModelForm):
 
     class Meta:
         model = AppSettings
-        fields = ['likes_sports', 'political_bias', 'likes_books', 'likes_music', 'likes_restaurants', 'likes_games']
+        fields = ['likes_sports', 'likes_books', 'likes_music', 'likes_restaurants', 'likes_games', 'political_bias']
+        widgets = {
+            'political_bias': Select(choices=AppSettings.POLITICAL_CHOICES)
+        }
