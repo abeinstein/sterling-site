b9cd1f6a73aafb465dc5475c68f94cc58d9f8194
mlevy <mlevy@uchicago.edu>
2013-11-14 13:08:10 -0600

    Removed political bias from settings form

--- sterling/apps/forms.py
+++ sterling/apps/forms.py
@@ -15,7 +15,7 @@ CITIES = (  (None, "----"),
 class AppSettingsForm(ModelForm):
     class Meta:
         model = AppSettings
-        fields = ['likes_sports', 'likes_books', 'likes_music', 'likes_restaurants', 'likes_games', 'political_bias', 'city', 'social_circle']
+        fields = ['likes_sports', 'likes_books', 'likes_music', 'likes_restaurants', 'likes_games', 'city', 'social_circle']
         widgets = {
             'political_bias': Select(choices=AppSettings.POLITICAL_CHOICES),
             'city': Select(choices=CITIES),
